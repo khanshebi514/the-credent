664154b46fb0548aa1dd970b0b917d0f9f29036b
khanshebi514 <khanshebi514@gmail.com>
2024-07-18 19:31:56 +0500

    18/07/2024 07:30 PM

---
+++ src/Home/sections/what-set-apart.js
@@ -0,0 +1,63 @@
+export default function WhatSetApart() {
+  return (
+    <section className="bg-white w-full ">
+        <div className="max-w-[1280px] m-auto mt-5 md:mt-[50px] w-full">
+            <div className="w-[80%] h-[2px] bg-gray-300 shadow-lg m-auto mb-5 md:mb-[50px]"></div>
+            <h1 className="text-xl md:text-3xl ml-16">
+                WHAT SETS <span className="font-bold">US APART</span>
+            </h1>
+
+<div className="flex justify-center gap-5 flex-col md:flex-row px-5 w-full items-center md:items-start">
+
+<div className="flex justify-between items-center gap-x-10 flex-wrap w-[60%]">
+                <div className="flex justify-center items-start gap-2 flex-col w-[320px] h-[148px]">
+                <h2 className="text-lg md:text-xl font-bold">Expertise</h2>
+                <p className="text-gray-400">Our in-depth industry knowledge ensures precision and compliance in every aspect of credentialing.</p>
+                </div>
+
+                <div className="flex justify-center items-start gap-2 flex-col w-[320px] h-[148px]">
+                    <h2 className="text-lg md:text-xl font-bold">Dedication</h2>
+                    <p className="text-gray-400">We're committed to your success, working tirelessly to exceed your expectations and goals.</p>
+                </div>
+                
+                <div className="flex justify-center items-start gap-2 flex-col w-[320px] h-[148px]">
+                    <h2 className="text-lg md:text-xl font-bold">Efficieny</h2>
+                    <p className="text-gray-400">We streamline the process, saving you time, reducing delays, and accelerating provider approvals.</p>
+                </div>
+
+                <div className="flex justify-center items-start gap-2 flex-col w-[320px] h-[148px]">
+                    <h2 className="text-lg md:text-xl font-bold">Pocket Friendly</h2>
+                    <p className="text-gray-400">We provide our services at a competitive rate of $149 per application.</p>
+                </div>
+
+                <div className="flex justify-center items-start gap-2 flex-col w-[320px] h-[148px]">
+                    <h2 className="text-lg md:text-xl font-bold">Proactive</h2>
+                    <p className="text-gray-400">Our team anticipates changes and needs, staying ahead of industry shifts and ensuring long-term success.</p>
+                </div>
+
+                <div className="flex justify-center items-start gap-2 flex-col w-[320px] h-[148px]">
+                    <h2 className="text-lg md:text-xl font-bold">Comprehensive</h2>
+                    <p className="text-gray-400">
+                    We handle initial credentialing, re-credentialing, and all related tasks with unparalleled thoroughness.
+                    </p>
+                </div>
+            </div>
+
+            <div>
+                <h2>and more...</h2>
+
+                <div className="w-[330px] bg-gray-200 p-1 rounded-t-md">
+                    <h2 className="py-2 font-bold text-lg ml-2">A Gateway to Credentialing</h2>
+                    <div className="p-10 bg-white">
+                        <p className="text-gray-400 text-sm">Credentialing Services provide a gateway for your insurance to remove complexities, unlock new opportunities, maximize your revenue, and maintain your credentials through streamlining billing, secure reimbursement, negotiating strategies, and uninterrupted practice growth.</p>
+                    </div>
+                </div>
+            </div>
+</div>
+          
+
+        </div>
+
+    </section>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import DosnDont from "@/Home/DosnDonts";
+import DontSettleOrdinary from "@/Home/sections/dont-settle-ordinary";
 import GovtInsurance from "@/Home/sections/Govt-insurance";
 import GurantedSection from "@/Home/sections/Guranted-section";
 import Hero from "@/Home/sections/HeroSection";
 import OffersSections from "@/Home/sections/offers-sections";
 import ServiceOffer from "@/Home/sections/services-offer";
 import StremLiningCred from "@/Home/sections/StremLiningCred";
+import WhatSetApart from "@/Home/sections/what-set-apart";
 import WhenCredentialing from "@/Home/sections/WhenCredentialing";
 
 export default function Home() {
@@ -18,6 +20,8 @@ export default function Home() {
     <ServiceOffer/>
     <GovtInsurance/>
     <OffersSections/>
+    <DontSettleOrdinary/>
+    <WhatSetApart/>
    </main>
   );
 }
