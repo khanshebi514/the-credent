f0690213c04892a748367649f39cc3a445ca6c4d
khanshebi514 <khanshebi514@gmail.com>
2024-08-05 17:50:43 +0500

    Final Changes with minor updates

--- src/app/services/commercial-insurance-credentialing/page.js
+++ src/app/services/commercial-insurance-credentialing/page.js
@@ -1,8 +1,8 @@
-import CommInnsuServ from "@/pages/services/commercial-insurance-credenciling/CommInnsuServ";
+
 import CredProcessSec from "@/pages/services/commercial-insurance-credenciling/CredProcessSec";
 import IntroToCommercialCred from "@/pages/services/commercial-insurance-credenciling/IntroToCommercial";
 
-export default function page() {
+export default function CommercialInsuranceCred() {
   return (
     <main>
          <IntroToCommercialCred/>
